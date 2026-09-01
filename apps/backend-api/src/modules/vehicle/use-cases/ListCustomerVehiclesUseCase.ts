6611c7a4a2f30ce470504702bd7cc9de282f9192
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 16:41:03 +0530

    refactor(vehicle): remove transport-owned list use case

--- apps/backend-api/src/modules/vehicle/use-cases/ListCustomerVehiclesUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { IVehicleRepository, Vehicle } from '@carbroz/domain-garage';
-
-export class ListCustomerVehiclesUseCase {
-  constructor(private readonly vehicleRepository: IVehicleRepository) {}
-
-  async execute(customerId: number): Promise<Vehicle[]> {
-    return this.vehicleRepository.listByCustomerId(customerId);
-  }
-}
