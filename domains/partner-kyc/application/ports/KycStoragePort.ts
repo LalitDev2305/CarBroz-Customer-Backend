693feb0a5d5877cef5001864fe8d4f5f625c660f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 17:10:57 +0530

    refactor(partner-kyc): define storage application port

---
+++ domains/partner-kyc/application/ports/KycStoragePort.ts
@@ -0,0 +1,16 @@
+export interface KycFileValidationOptions {
+  maxSizeBytes: number;
+  allowedMimeTypes: readonly string[];
+}
+
+export interface KycPresignedDownloadOptions {
+  bucket: string;
+  objectName: string;
+  expiresInSeconds: number;
+}
+
+export interface KycStoragePort {
+  validateFile(buffer: Buffer, mimeType: string, options: KycFileValidationOptions): Promise<boolean>;
+  uploadFile(bucket: string, objectName: string, buffer: Buffer, mimeType: string): Promise<string>;
+  getPresignedDownloadUrl(options: KycPresignedDownloadOptions): Promise<string>;
+}
