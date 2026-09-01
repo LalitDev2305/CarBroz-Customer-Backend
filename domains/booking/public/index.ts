17580628877f078ec6d1f56655e84fd8a1005e04
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 15:38:17 +0530

    refactor(booking): remove slot-availability ownership leak

--- domains/booking/public/index.ts
+++ domains/booking/public/index.ts
@@ -4,6 +4,5 @@ export * from '../domain/BookingStatus.js';
 export * from '../domain/BookingStatusHistoryItem.js';
 export * from '../domain/repositories/IBookingRepository.js';
 export * from '../infrastructure/repositories/PrismaBookingRepository.js';
-export * from '../application/GetAvailableSlotsUseCase.js';
 export * from '../booking.module.js';
 export * from '../module.manifest.js';
