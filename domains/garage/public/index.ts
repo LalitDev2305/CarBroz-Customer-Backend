8b373a5d7dccfefc43ee375bb9cd8f9586c43188
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 16:37:54 +0530

    refactor(garage): publish bounded-context vehicle contracts

--- domains/garage/public/index.ts
+++ domains/garage/public/index.ts
@@ -1,5 +1,4 @@
 export * from '../domain/Vehicle.js';
 export * from '../domain/VehicleStatus.js';
-export * from '../infrastructure/repositories/PrismaVehicleRepository.js';
+export * from '../domain/repositories/IVehicleRepository.js';
 export * from '../garage.module.js';
-export * from '../module.manifest.js';
