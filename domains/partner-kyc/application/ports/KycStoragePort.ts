e4ca85f6f07fdc5add303475bb425e90cffbf665
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 17:16:02 +0530

    refactor(partner-kyc): keep storage port structurally compatible

--- domains/partner-kyc/application/ports/KycStoragePort.ts
+++ domains/partner-kyc/application/ports/KycStoragePort.ts
@@ -1,6 +1,6 @@
 export interface KycFileValidationOptions {
   maxSizeBytes: number;
-  allowedMimeTypes: readonly string[];
+  allowedMimeTypes: string[];
 }
 
 export interface KycPresignedDownloadOptions {
