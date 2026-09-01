82af03bcbd767ac68283ffd699e347609aa0e52f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 19:23:58 +0530

    refactor(database): stop exporting duplicate KYC repository

--- platform/database/src/index.ts
+++ platform/database/src/index.ts
@@ -12,7 +12,6 @@ export * from './repositories/PrismaFeatureFlagRepository.js';
 export * from './repositories/PrismaPartnerRepository.js';
 export * from './repositories/PrismaPartnerMemberRepository.js';
 export * from './repositories/PrismaPartnerProfileRepository.js';
-export * from './repositories/PrismaKycDocumentRepository.js';
 export * from './repositories/PrismaCustomerProfileRepository.js';
 export * from './repositories/PrismaAddressRepository.js';
 export * from './repositories/PrismaCatalogRepository.js';
