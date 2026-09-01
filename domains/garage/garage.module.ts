c146d348ab13e56dd07339b49ad15888ab3d7a87
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-01 16:40:29 +0530

    refactor(garage): compose vehicle application services

--- domains/garage/garage.module.ts
+++ domains/garage/garage.module.ts
@@ -1,4 +1,8 @@
-import { asFunction, type AwilixContainer } from 'awilix';
+import { asClass, asFunction, type AwilixContainer } from 'awilix';
+import { ArchiveVehicleUseCase } from './application/use-cases/ArchiveVehicleUseCase.js';
+import { CreateVehicleUseCase } from './application/use-cases/CreateVehicleUseCase.js';
+import { ListCustomerVehiclesUseCase } from './application/use-cases/ListCustomerVehiclesUseCase.js';
+import { SetDefaultVehicleUseCase } from './application/use-cases/SetDefaultVehicleUseCase.js';
 import type { VehiclePersistenceClient } from './infrastructure/persistence/VehiclePersistenceClient.js';
 import { PrismaVehicleRepository } from './infrastructure/repositories/PrismaVehicleRepository.js';
 
@@ -15,5 +19,9 @@ export function registerGarageModule(container: AwilixContainer): void {
     vehicleRepository: asFunction(
       (cradle: GarageCradle) => new PrismaVehicleRepository(cradle.prismaProvider.getClient()),
     ).singleton(),
+    createVehicleUseCase: asClass(CreateVehicleUseCase).classic().scoped(),
+    listCustomerVehiclesUseCase: asClass(ListCustomerVehiclesUseCase).classic().scoped(),
+    setDefaultVehicleUseCase: asClass(SetDefaultVehicleUseCase).classic().scoped(),
+    archiveVehicleUseCase: asClass(ArchiveVehicleUseCase).classic().scoped(),
   });
 }
