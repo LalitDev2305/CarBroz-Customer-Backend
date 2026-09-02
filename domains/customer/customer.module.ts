b4fe25652d1cfe42878f188c74d2ce54b28d8f4b
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-02 19:11:54 +0530

    refactor(customer): make customer module the single composition authority

--- domains/customer/customer.module.ts
+++ domains/customer/customer.module.ts
@@ -1,6 +1,10 @@
 import type { AwilixContainer } from 'awilix';
+import { registerAddressModule } from './address/address.module.js';
 import { registerGarageModule } from './garage/garage.module.js';
+import { registerCustomerProfileModule } from './profile/profile.module.js';
 
 export function registerCustomerModule(container: AwilixContainer): void {
+  registerCustomerProfileModule(container);
+  registerAddressModule(container);
   registerGarageModule(container);
 }
