9503dfd62e1fec42c31547f4025e381cbc651c95
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-03 19:20:12 +0530

    refactor(identity): remove UI navigation decisions from identity

--- domains/identity/application/SendOtpUseCase.ts
+++ domains/identity/application/SendOtpUseCase.ts
@@ -7,26 +7,16 @@ interface Input {
 }
 
 export class SendOtpUseCase implements IUseCase<Input, any> {
-  constructor(
-    private readonly userRepository: IUserRepository
-  ) {}
+  constructor(private readonly userRepository: IUserRepository) {}
 
   async execute(input: Input): Promise<any> {
     const { phoneNumber } = input;
-    
     const user = await this.userRepository.findByPhoneNumber(phoneNumber);
-    const isNewUser = !user;
 
-    const mockOtp = '123456';
-    
     return {
       message: 'OTP sent successfully',
-      mockOtp,
-      isNewUser,
-      nextScreen: {
-        template: 'form_template',
-        api: 'auth/auth_otp'
-      }
+      mockOtp: '123456',
+      isNewUser: !user,
     };
   }
 }
