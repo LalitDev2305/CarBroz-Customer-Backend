285eebb57dcb69436b9fceebb979e72499eaa52e
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:41:14 +0530

    refactor(common): forward aggregate contract to foundation

--- packages/common/src/domain/IAggregateRoot.ts
+++ packages/common/src/domain/IAggregateRoot.ts
@@ -1,5 +1,1 @@
-import { IEntity } from './IEntity.js';
-
-export interface IAggregateRoot<TId> extends IEntity<TId> {
-  // Marker interface for Aggregate Roots
-}
+export type { IAggregateRoot } from '@carbroz/foundation-kernel';
