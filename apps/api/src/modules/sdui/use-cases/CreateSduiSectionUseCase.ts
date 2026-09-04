971604b9505a512c6026e45fca96e8caba7e56b9
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:07:22 +0530

    refactor(api): delegate SDUI section registration to registry

--- apps/api/src/modules/sdui/use-cases/CreateSduiSectionUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/CreateSduiSectionUseCase.ts
@@ -1,20 +1,3 @@
-import { ForbiddenError, IRequestContext, IUseCase } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiSectionEntity } from '@carbroz/sdui-registry';
-import type { CreateSduiSectionDto } from '../dtos/sdui-registry.dto.js';
-
-export interface CreateSduiSectionInput {
-  context: IRequestContext;
-  data: CreateSduiSectionDto;
-}
-
-export class CreateSduiSectionUseCase implements IUseCase<CreateSduiSectionInput, SduiSectionEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: CreateSduiSectionInput): Promise<SduiSectionEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can create SDUI sections');
-    }
-
-    return this.sduiRegistryRepository.createSection(input.data);
-  }
-}
+/** @deprecated Import CreateSduiSectionUseCase from @carbroz/sdui-registry. */
+export { CreateSduiSectionUseCase } from '@carbroz/sdui-registry';
+export type { RegistryNodeCommandInput as CreateSduiSectionInput } from '@carbroz/sdui-registry';
