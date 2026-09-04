77d623ae15db997c1238dcb3245061d58a45e0b8
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:08:15 +0530

    refactor(api): delegate SDUI rollback to registry

--- apps/api/src/modules/sdui/use-cases/RollbackSduiVersionUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/RollbackSduiVersionUseCase.ts
@@ -1,23 +1,3 @@
-import { IUseCase, IRequestContext, ForbiddenError } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiScreenEntity } from '@carbroz/sdui-registry';
-import { RollbackSduiVersionDto } from '../dtos/sdui-registry.dto.js';
-
-export interface RollbackSduiVersionInput {
-  context: IRequestContext;
-  data: RollbackSduiVersionDto;
-}
-
-export class RollbackSduiVersionUseCase implements IUseCase<RollbackSduiVersionInput, SduiScreenEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: RollbackSduiVersionInput): Promise<SduiScreenEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can rollback SDUI versions');
-    }
-
-    const { screenId, targetApp = 'CUSTOMER', targetVersionNumber } = input.data;
-    const publisherName = `user-${input.context.authenticatedUser.id}`;
-
-    return await this.sduiRegistryRepository.rollbackVersion(screenId, targetApp, targetVersionNumber, publisherName);
-  }
-}
+/** @deprecated Import RollbackSduiVersionUseCase from @carbroz/sdui-registry. */
+export { RollbackSduiVersionUseCase } from '@carbroz/sdui-registry';
+export type { RollbackSduiVersionInput, RollbackSduiVersionRequest } from '@carbroz/sdui-registry';
