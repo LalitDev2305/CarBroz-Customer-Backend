8b29627ad7568937e8aefb0a4c7623c683fac25f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:52:02 +0530

    refactor(common): forward feature flag repository to configuration domain

--- packages/common/src/domain/repositories/IFeatureFlagRepository.ts
+++ packages/common/src/domain/repositories/IFeatureFlagRepository.ts
@@ -1,7 +1,1 @@
-import { IRepository } from '../IRepository.js';
-import { FeatureFlag } from '../FeatureFlag.js';
-
-export interface IFeatureFlagRepository extends IRepository<FeatureFlag, number> {
-  findByKey(key: string): Promise<FeatureFlag | null>;
-  findAllFlags(): Promise<FeatureFlag[]>;
-}
+export type { IFeatureFlagRepository } from '@carbroz/domain-configuration';
