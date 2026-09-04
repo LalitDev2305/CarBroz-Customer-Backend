902db759ee2e49b7c182c7066bb7cdff3331609b
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 11:00:29 +0530

    refactor(common): forward admin role repository to identity domain

--- packages/common/src/domain/repositories/IAdminRoleRepository.ts
+++ packages/common/src/domain/repositories/IAdminRoleRepository.ts
@@ -1,7 +1,1 @@
-import { AdminUserRole } from '../AdminUserRole.js';
-
-export interface IAdminRoleRepository {
-  assignRole(userId: number, roleId: number, assignedBy?: number): Promise<AdminUserRole>;
-  removeRole(userId: number, roleId: number): Promise<boolean>;
-  findRolesForUser(userId: number): Promise<number[]>;
-}
+export type { IAdminRoleRepository } from '@carbroz/domain-identity';
