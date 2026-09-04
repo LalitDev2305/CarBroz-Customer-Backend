45d406add03eb9f97c93d2f244d630a1dd363453
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:52:09 +0530

    refactor(common): forward config provider to configuration domain

--- packages/common/src/providers/IConfigProvider.ts
+++ packages/common/src/providers/IConfigProvider.ts
@@ -1,7 +1,1 @@
-import { IProvider } from './IProvider.js';
-
-export interface IConfigProvider extends IProvider {
-  get<T>(key: string, defaultValue?: T): Promise<T>;
-  has(key: string): Promise<boolean>;
-  getAll(): Promise<Record<string, string>>;
-}
+export type { IConfigProvider } from '@carbroz/domain-configuration';
