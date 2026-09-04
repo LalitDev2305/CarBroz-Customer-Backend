ffdd0568b52c89e6d890d46a76eb0e1ea815175f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:59:16 +0530

    refactor(common): forward user to identity domain

--- packages/common/src/domain/User.ts
+++ packages/common/src/domain/User.ts
@@ -1,11 +1,1 @@
-export interface User {
-  id: number;
-  publicId: string;
-  email: string | null;
-  phoneNumber: string | null;
-  isGuest: boolean;
-  role: string;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date | null;
-}
+export type { User } from '@carbroz/domain-identity';
