326acbdaeffdda6e464fdc58b552d47a48b5047c
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:51:54 +0530

    refactor(common): forward config repository to configuration domain

--- packages/common/src/domain/repositories/IConfigRepository.ts
+++ packages/common/src/domain/repositories/IConfigRepository.ts
@@ -1,7 +1,1 @@
-import { IRepository } from '../IRepository.js';
-import { SystemConfig } from '../SystemConfig.js';
-
-export interface IConfigRepository extends IRepository<SystemConfig, number> {
-  findByKey(key: string): Promise<SystemConfig | null>;
-  findAllConfig(): Promise<SystemConfig[]>;
-}
+export type { IConfigRepository } from '@carbroz/domain-configuration';
