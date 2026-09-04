8a5a5908b1870a9b5867a4314680e7b800eac663
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:08:37 +0530

    refactor(api): delegate SDUI version lookup to registry

--- apps/api/src/modules/sdui/use-cases/GetSduiSpecificVersionUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/GetSduiSpecificVersionUseCase.ts
@@ -1,24 +1,3 @@
-import { IUseCase, NotFoundError } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiScreenEntity } from '@carbroz/sdui-registry';
-import type { SduiTargetApp } from '@carbroz/ui-sdk';
-
-export interface GetSduiSpecificVersionInput {
-  screenId: string;
-  targetApp?: SduiTargetApp;
-  versionNumber: number;
-}
-
-export class GetSduiSpecificVersionUseCase implements IUseCase<GetSduiSpecificVersionInput, SduiScreenEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: GetSduiSpecificVersionInput): Promise<SduiScreenEntity> {
-    const { screenId, targetApp = 'CUSTOMER', versionNumber } = input;
-    const version = await this.sduiRegistryRepository.getSpecificVersion(screenId, targetApp, versionNumber);
-
-    if (!version) {
-      throw new NotFoundError(`Screen version ${versionNumber} not found for screen '${screenId}'`);
-    }
-
-    return version;
-  }
-}
+/** @deprecated Import GetSduiSpecificVersionUseCase from @carbroz/sdui-registry. */
+export { GetSduiSpecificVersionUseCase } from '@carbroz/sdui-registry';
+export type { GetSduiSpecificVersionInput } from '@carbroz/sdui-registry';
