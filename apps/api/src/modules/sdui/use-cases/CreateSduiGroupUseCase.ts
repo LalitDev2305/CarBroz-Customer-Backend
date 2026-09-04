3a3ce672ba6ba1d1df25dcd79c12acb997933e06
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:07:30 +0530

    refactor(api): delegate SDUI group registration to registry

--- apps/api/src/modules/sdui/use-cases/CreateSduiGroupUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/CreateSduiGroupUseCase.ts
@@ -1,20 +1,3 @@
-import { ForbiddenError, IRequestContext, IUseCase } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiGroupEntity } from '@carbroz/sdui-registry';
-import type { CreateSduiGroupDto } from '../dtos/sdui-registry.dto.js';
-
-export interface CreateSduiGroupInput {
-  context: IRequestContext;
-  data: CreateSduiGroupDto;
-}
-
-export class CreateSduiGroupUseCase implements IUseCase<CreateSduiGroupInput, SduiGroupEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: CreateSduiGroupInput): Promise<SduiGroupEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can create SDUI groups');
-    }
-
-    return this.sduiRegistryRepository.createGroup(input.data);
-  }
-}
+/** @deprecated Import CreateSduiGroupUseCase from @carbroz/sdui-registry. */
+export { CreateSduiGroupUseCase } from '@carbroz/sdui-registry';
+export type { RegistryNodeCommandInput as CreateSduiGroupInput } from '@carbroz/sdui-registry';
