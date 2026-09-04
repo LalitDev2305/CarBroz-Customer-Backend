7ca6c3c877309b182fa4c468e4b6f0c752d38d96
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:51:41 +0530

    refactor(common): forward system config to configuration domain

--- packages/common/src/domain/SystemConfig.ts
+++ packages/common/src/domain/SystemConfig.ts
@@ -1,10 +1,1 @@
-export interface SystemConfig {
-  id: number;
-  publicId: string;
-  key: string;
-  value: string;
-  description?: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt?: Date | null;
-}
+export type { SystemConfig } from '@carbroz/domain-configuration';
