1a570ccfa8a7eb13b9ae0d6d518b96d00677dd77
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 11:00:15 +0530

    refactor(common): forward role repository to identity domain

--- packages/common/src/domain/repositories/IRoleRepository.ts
+++ packages/common/src/domain/repositories/IRoleRepository.ts
@@ -1,8 +1,1 @@
-import { Role } from '../Role.js';
-import { IReadRepository } from '../IReadRepository.js';
-import { IWriteRepository } from '../IWriteRepository.js';
-
-export interface IRoleRepository extends IReadRepository<Role, number>, IWriteRepository<Role, number> {
-  findByName(name: string): Promise<Role | null>;
-  findWithPermissions(roleId: number): Promise<Role & { permissions: number[] } | null>;
-}
+export type { IRoleRepository } from '@carbroz/domain-identity';
