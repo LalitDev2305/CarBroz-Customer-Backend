290198d90a30f4733c08e7c59e5fb1b99c32428b
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:37:03 +0530

    refactor(config): remove migrated redis environment schema

--- packages/config/src/RedisConfig.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const redisSchema = z.object({
-  REDIS_URL: z.string().url().default('redis://localhost:6379'),
-});
-
-export type RedisConfigType = z.infer<typeof redisSchema>;
