b7c87d565d8a6d9395c3591c9e415b09fac8c0ef
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:07:55 +0530

    refactor(api): delegate SDUI draft updates to registry

--- apps/api/src/modules/sdui/use-cases/UpdateSduiDraftUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/UpdateSduiDraftUseCase.ts
@@ -1,20 +1,3 @@
-import { IUseCase, IRequestContext, ForbiddenError } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiScreenEntity } from '@carbroz/sdui-registry';
-import { UpdateSduiDraftDto } from '../dtos/sdui-registry.dto.js';
-
-export interface UpdateSduiDraftInput {
-  context: IRequestContext;
-  data: UpdateSduiDraftDto;
-}
-
-export class UpdateSduiDraftUseCase implements IUseCase<UpdateSduiDraftInput, SduiScreenEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: UpdateSduiDraftInput): Promise<SduiScreenEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can update SDUI drafts');
-    }
-
-    return await this.sduiRegistryRepository.updateDraft(input.data);
-  }
-}
+/** @deprecated Import UpdateSduiDraftUseCase from @carbroz/sdui-registry. */
+export { UpdateSduiDraftUseCase } from '@carbroz/sdui-registry';
+export type { UpdateSduiDraftInput } from '@carbroz/sdui-registry';
