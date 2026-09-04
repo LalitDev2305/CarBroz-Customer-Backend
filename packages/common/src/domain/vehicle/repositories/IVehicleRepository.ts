5cc1ed00d9830fd0f126490e7f093d4c2406b37f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 11:28:14 +0530

    refactor(common): forward IVehicleRepository to canonical customer domain

--- packages/common/src/domain/vehicle/repositories/IVehicleRepository.ts
+++ packages/common/src/domain/vehicle/repositories/IVehicleRepository.ts
@@ -1,12 +1,1 @@
-import { Vehicle } from '../Vehicle.js';
-
-export interface IVehicleRepository {
-  create(vehicle: Vehicle): Promise<Vehicle>;
-  findById(id: number): Promise<Vehicle | null>;
-  findByPublicId(publicId: string): Promise<Vehicle | null>;
-  findByCustomerAndRegistration(customerId: number, registrationNumber: string): Promise<Vehicle | null>;
-  listByCustomerId(customerId: number): Promise<Vehicle[]>;
-  update(vehicle: Vehicle): Promise<Vehicle>;
-  unsetCustomerDefaultVehicles(customerId: number, excludeVehicleId?: number): Promise<void>;
-  softDelete(id: number): Promise<void>;
-}
+export type { IVehicleRepository } from '@carbroz/domain-customer';
