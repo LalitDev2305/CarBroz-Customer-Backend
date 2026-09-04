f0121ec58ec1f615b27d4da82e7d9c0515936bb2
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:07:39 +0530

    refactor(api): delegate SDUI element registration to registry

--- apps/api/src/modules/sdui/use-cases/CreateSduiElementUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/CreateSduiElementUseCase.ts
@@ -1,20 +1,3 @@
-import { ForbiddenError, IRequestContext, IUseCase } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiElementEntity } from '@carbroz/sdui-registry';
-import type { CreateSduiElementDto } from '../dtos/sdui-registry.dto.js';
-
-export interface CreateSduiElementInput {
-  context: IRequestContext;
-  data: CreateSduiElementDto;
-}
-
-export class CreateSduiElementUseCase implements IUseCase<CreateSduiElementInput, SduiElementEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: CreateSduiElementInput): Promise<SduiElementEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can create SDUI elements');
-    }
-
-    return this.sduiRegistryRepository.createElement(input.data);
-  }
-}
+/** @deprecated Import CreateSduiElementUseCase from @carbroz/sdui-registry. */
+export { CreateSduiElementUseCase } from '@carbroz/sdui-registry';
+export type { RegistryNodeCommandInput as CreateSduiElementInput } from '@carbroz/sdui-registry';
