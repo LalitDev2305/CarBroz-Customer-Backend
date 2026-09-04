189f6547bf7ef3c79484aa243a501c6bba192f09
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-04 10:52:16 +0530

    refactor(common): forward feature flag provider to configuration domain

--- packages/common/src/providers/IFeatureFlagProvider.ts
+++ packages/common/src/providers/IFeatureFlagProvider.ts
@@ -1,6 +1,1 @@
-import { IProvider } from './IProvider.js';
-
-export interface IFeatureFlagProvider extends IProvider {
-  isEnabled(key: string): Promise<boolean>;
-  getAllFlags(): Promise<Record<string, boolean>>;
-}
+export type { IFeatureFlagProvider } from '@carbroz/domain-configuration';
