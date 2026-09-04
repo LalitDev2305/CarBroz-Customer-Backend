b218a09424ff47e68d28b25e8b4ec650b674502e
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 03:08:09 +0530

    refactor(api): delegate SDUI archival to registry

--- apps/api/src/modules/sdui/use-cases/ArchiveSduiVersionUseCase.ts
+++ apps/api/src/modules/sdui/use-cases/ArchiveSduiVersionUseCase.ts
@@ -1,22 +1,3 @@
-import { IUseCase, IRequestContext, ForbiddenError } from '@carbroz/common';
-import type { ISduiRegistryRepository, SduiScreenEntity } from '@carbroz/sdui-registry';
-import { ArchiveSduiVersionDto } from '../dtos/sdui-registry.dto.js';
-
-export interface ArchiveSduiVersionInput {
-  context: IRequestContext;
-  data: ArchiveSduiVersionDto;
-}
-
-export class ArchiveSduiVersionUseCase implements IUseCase<ArchiveSduiVersionInput, SduiScreenEntity> {
-  constructor(private readonly sduiRegistryRepository: ISduiRegistryRepository) {}
-
-  public async execute(input: ArchiveSduiVersionInput): Promise<SduiScreenEntity> {
-    if (!input.context.authenticatedUser?.isAdmin) {
-      throw new ForbiddenError('Only administrators can archive SDUI versions');
-    }
-
-    const { screenId, targetApp = 'CUSTOMER', versionNumber } = input.data;
-
-    return await this.sduiRegistryRepository.archiveVersion(screenId, targetApp, versionNumber);
-  }
-}
+/** @deprecated Import ArchiveSduiVersionUseCase from @carbroz/sdui-registry. */
+export { ArchiveSduiVersionUseCase } from '@carbroz/sdui-registry';
+export type { ArchiveSduiVersionInput, ArchiveSduiVersionRequest } from '@carbroz/sdui-registry';
