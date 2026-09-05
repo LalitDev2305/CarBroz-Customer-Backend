d4e265895451fdc6a7e275a0de8d4a02899f8a7c
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 11:10:48 +0530

    refactor(api): make payout listing compatibility-only

--- apps/api/src/modules/payout/use-cases/ListPartnerPayoutsUseCase.ts
+++ apps/api/src/modules/payout/use-cases/ListPartnerPayoutsUseCase.ts
@@ -1,9 +1,1 @@
-import { IPartnerPayoutRepository, PartnerPayout, PayoutStatus } from '@carbroz/common';
-
-export class ListPartnerPayoutsUseCase {
-  constructor(private readonly payoutRepository: IPartnerPayoutRepository) {}
-
-  async execute(partnerId: number, status?: PayoutStatus): Promise<PartnerPayout[]> {
-    return await this.payoutRepository.listByPartnerId(partnerId, status);
-  }
-}
+export { ListPartnerPayoutsUseCase } from '@carbroz/domain-financials';
