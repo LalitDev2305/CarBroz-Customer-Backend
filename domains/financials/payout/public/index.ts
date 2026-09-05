544a1ca3947fee291a606d1ccb5ba23962a15629
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 11:05:18 +0530

    refactor(financials): keep payout infrastructure private

--- domains/financials/payout/public/index.ts
+++ domains/financials/payout/public/index.ts
@@ -1,6 +1,5 @@
 export * from '../domain/PartnerPayout.js';
 export * from '../domain/PayoutStatus.js';
 export * from '../domain/repositories/IPartnerPayoutRepository.js';
-export * from '../infrastructure/repositories/PrismaPartnerPayoutRepository.js';
 export * from '../payout.module.js';
 export * from '../module.manifest.js';
