23497f44eb9b0fcfb38fa1abd41c851cd659f8eb
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-05 11:10:38 +0530

    refactor(api): make get invoice compatibility-only

--- apps/api/src/modules/invoice/use-cases/GetInvoiceUseCase.ts
+++ apps/api/src/modules/invoice/use-cases/GetInvoiceUseCase.ts
@@ -1,13 +1,1 @@
-import { IInvoiceRepository, Invoice } from '@carbroz/common';
-
-export class GetInvoiceUseCase {
-  constructor(private readonly invoiceRepository: IInvoiceRepository) {}
-
-  async execute(publicId: string): Promise<Invoice> {
-    const invoice = await this.invoiceRepository.findByPublicId(publicId);
-    if (!invoice) {
-      throw new Error('Invoice not found');
-    }
-    return invoice;
-  }
-}
+export { GetInvoiceUseCase } from '@carbroz/domain-financials';
