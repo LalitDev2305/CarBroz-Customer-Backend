1a04158393bb31364e6178196140516380efa404
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-09 23:03:22 +0530

    feat(sdui): complete image semantic builder API

--- sdui/ui-sdk/src/definitions/elements/image/image.builder.ts
+++ sdui/ui-sdk/src/definitions/elements/image/image.builder.ts
@@ -4,19 +4,25 @@ import { ElementFactory } from '../../../factory/NodeFactories.js';
 import type { ImageProperties } from './image.properties.js';
 
 export class ImageBuilder extends TypedElementBuilder<ImageProperties> {
-  constructor(id: string, url?: string) {
+  constructor(id: string, source?: string) {
     super(id);
-    if (url !== undefined) this.source(url);
+    if (source !== undefined) this.source(source);
   }
 
-  source(url: string): this { return this.setProperty('url', url); }
+  source(value: string): this { return this.setProperty('url', value); }
   width(value: ImageProperties['width']): this { return this.setProperty('width', value); }
   height(value: ImageProperties['height']): this { return this.setProperty('height', value); }
+  size(width: ImageProperties['width'], height: ImageProperties['height']): this {
+    this.setProperty('width', width);
+    return this.setProperty('height', height);
+  }
+  minWidth(value: ImageProperties['minWidth']): this { return this.setProperty('minWidth', value); }
+  minHeight(value: ImageProperties['minHeight']): this { return this.setProperty('minHeight', value); }
   maxWidth(value: ImageProperties['maxWidth']): this { return this.setProperty('maxWidth', value); }
   maxHeight(value: ImageProperties['maxHeight']): this { return this.setProperty('maxHeight', value); }
+  contentScale(value: ImageProperties['contentScale']): this { return this.setProperty('contentScale', value); }
   fillMaxWidth(value = true): this { return this.setProperty('fillMaxWidth', value); }
   fillMaxHeight(value = true): this { return this.setProperty('fillMaxHeight', value); }
-  contentScale(value: ImageProperties['contentScale']): this { return this.setProperty('contentScale', value); }
 
   build(): SduiElement { return ElementFactory.create('image', this.elementInput()); }
 }
