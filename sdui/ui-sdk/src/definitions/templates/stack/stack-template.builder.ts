766b06858827604d07bf0d8239bc4b48984cbbd4
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-09 23:01:58 +0530

    feat(sdui): make template own component builders

--- sdui/ui-sdk/src/definitions/templates/stack/stack-template.builder.ts
+++ sdui/ui-sdk/src/definitions/templates/stack/stack-template.builder.ts
@@ -1,14 +1,19 @@
-import type { SduiComponent } from '../../../contract/component.schema.js';
 import type { SduiTemplate } from '../../../contract/template.schema.js';
 import { TypedPropertyBuilder } from '../../../builder/TypedPropertyBuilder.js';
 import { TemplateFactory } from '../../../factory/NodeFactories.js';
+import { StackComponentBuilder } from '../../components/stack/stack-component.builder.js';
 import type { StackTemplateProperties } from './stack-template.properties.js';
 
+export type StackLikeTemplateType = 'stack_template' | 'form_template' | 'default_template';
+
 export class StackTemplateBuilder extends TypedPropertyBuilder<StackTemplateProperties> {
-  private readonly components: SduiComponent[] = [];
+  private readonly componentBuilders: StackComponentBuilder[] = [];
   private orientation: 'vertical' | 'horizontal' = 'vertical';
 
-  constructor(private readonly id: string) { super(); }
+  constructor(
+    private readonly id: string,
+    private readonly definitionType: StackLikeTemplateType = 'stack_template',
+  ) { super(); }
 
   vertical(): this { this.orientation = 'vertical'; return this.setProperty('orientation', 'vertical'); }
   horizontal(): this { this.orientation = 'horizontal'; return this.setProperty('orientation', 'horizontal'); }
@@ -22,17 +27,30 @@ export class StackTemplateBuilder extends TypedPropertyBuilder<StackTemplateProp
       ? this.setProperty('horizontalAlignment', 'center')
       : this.setProperty('verticalAlignment', 'center');
   }
+  horizontalAlignment(value: StackTemplateProperties['horizontalAlignment']): this { return this.setProperty('horizontalAlignment', value); }
+  verticalAlignment(value: StackTemplateProperties['verticalAlignment']): this { return this.setProperty('verticalAlignment', value); }
   padding(value: StackTemplateProperties['padding']): this { return this.setProperty('padding', value); }
+  width(value: StackTemplateProperties['width']): this { return this.setProperty('width', value); }
+  height(value: StackTemplateProperties['height']): this { return this.setProperty('height', value); }
+  maxWidth(value: StackTemplateProperties['maxWidth']): this { return this.setProperty('maxWidth', value); }
   fillMaxWidth(value = true): this { return this.setProperty('fillMaxWidth', value); }
   fillMaxHeight(value = true): this { return this.setProperty('fillMaxHeight', value); }
   fillMaxSize(value = true): this { return this.setProperty('fillMaxSize', value); }
-  addComponent(component: SduiComponent): this { this.components.push(component); return this; }
+  background(value: StackTemplateProperties['background']): this { return this.setProperty('background', value); }
+  border(value: StackTemplateProperties['border']): this { return this.setProperty('border', value); }
+  shape(value: StackTemplateProperties['shape']): this { return this.setProperty('shape', value); }
+
+  addStackComponent(id: string): StackComponentBuilder {
+    const component = new StackComponentBuilder(id);
+    this.componentBuilders.push(component);
+    return component;
+  }
 
   build(): SduiTemplate {
-    return TemplateFactory.create('stack_template', {
+    return TemplateFactory.create(this.definitionType, {
       id: this.id,
       properties: this.propertiesSnapshot(),
-      components: this.components,
+      components: this.componentBuilders.map((builder) => builder.build()),
     });
   }
 }
