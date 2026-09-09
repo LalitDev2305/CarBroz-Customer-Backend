4bcf31bc5e59adb40c5deed12854099a968b9058
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-10 02:35:36 +0530

    feat(sdui): register initial engine node vocabulary

---
+++ sdui/engine/src/nodes/definitions.ts
@@ -0,0 +1,21 @@
+import type { NodeDefinition } from '../core/NodeDefinition.js';
+import { StackComponentDefinition } from './component/StackComponent.js';
+import { ButtonDefinition } from './element/Button.js';
+import { ImageDefinition } from './element/Image.js';
+import { InputDefinition } from './element/Input.js';
+import { TextDefinition } from './element/Text.js';
+import { StackGroupDefinition } from './group/StackGroup.js';
+import { StackSectionDefinition } from './section/StackSection.js';
+import { StackTemplateDefinition } from './template/StackTemplate.js';
+
+/** Canonical reusable SDUI vocabulary currently required by the engine migration. */
+export const productionNodeDefinitions: readonly NodeDefinition[] = Object.freeze([
+  StackTemplateDefinition,
+  StackComponentDefinition,
+  StackSectionDefinition,
+  StackGroupDefinition,
+  TextDefinition,
+  ImageDefinition,
+  InputDefinition,
+  ButtonDefinition,
+]);
