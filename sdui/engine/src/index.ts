6522eb6a48283e79dc37e043e09aa43fa2bd77e5
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-10 02:29:24 +0530

    feat(sdui): expose core engine contracts

--- sdui/engine/src/index.ts
+++ sdui/engine/src/index.ts
@@ -1,5 +1,7 @@
+export * from './core/NodeDefinition.js';
 export * from './core/ScreenComposer.js';
 export * from './core/ScreenContext.js';
+export * from './core/SduiModel.js';
 export * from './core/SduiValidator.js';
 export * from './registry/ScreenRegistry.js';
 export * from './SduiService.js';
