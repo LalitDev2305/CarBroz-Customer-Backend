2973a34ff9e6704df783be88af0f5600f9b7baa2
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-10 02:17:33 +0530

    feat(sdui): expose validator through engine facade

--- sdui/engine/src/index.ts
+++ sdui/engine/src/index.ts
@@ -1,4 +1,5 @@
 export * from './core/ScreenComposer.js';
 export * from './core/ScreenContext.js';
+export * from './core/SduiValidator.js';
 export * from './registry/ScreenRegistry.js';
 export * from './SduiService.js';
