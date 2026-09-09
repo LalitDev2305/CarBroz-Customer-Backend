1cfe6f0853fc14f168b3dc802b30c65f3c2971b8
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-09 23:01:10 +0530

    feat(sdui): make stack group own element builders

--- sdui/ui-sdk/src/definitions/groups/stack/stack-group.builder.ts
+++ sdui/ui-sdk/src/definitions/groups/stack/stack-group.builder.ts
@@ -1,11 +1,9 @@
-import type { SduiElement } from '../../../contract/element.schema.js';
 import type { SduiGroup } from '../../../contract/group.schema.js';
-import { TypedPropertyBuilder } from '../../../builder/TypedPropertyBuilder.js';
+import { ElementParentBuilder } from '../../../builder/ElementParentBuilder.js';
 import { GroupFactory } from '../../../factory/NodeFactories.js';
 import type { StackGroupProperties } from './stack-group.properties.js';
 
-export class StackGroupBuilder extends TypedPropertyBuilder<StackGroupProperties> {
-  private readonly elements: SduiElement[] = [];
+export class StackGroupBuilder extends ElementParentBuilder<StackGroupProperties> {
   private orientation: 'vertical' | 'horizontal' = 'vertical';
 
   constructor(private readonly id: string) { super(); }
@@ -22,18 +20,25 @@ export class StackGroupBuilder extends TypedPropertyBuilder<StackGroupProperties
       ? this.setProperty('horizontalAlignment', 'center')
       : this.setProperty('verticalAlignment', 'center');
   }
+  verticalAlignment(value: StackGroupProperties['verticalAlignment']): this { return this.setProperty('verticalAlignment', value); }
+  horizontalAlignment(value: StackGroupProperties['horizontalAlignment']): this { return this.setProperty('horizontalAlignment', value); }
   padding(value: StackGroupProperties['padding']): this { return this.setProperty('padding', value); }
+  width(value: StackGroupProperties['width']): this { return this.setProperty('width', value); }
+  height(value: StackGroupProperties['height']): this { return this.setProperty('height', value); }
+  maxWidth(value: StackGroupProperties['maxWidth']): this { return this.setProperty('maxWidth', value); }
+  weight(value: StackGroupProperties['weight']): this { return this.setProperty('weight', value); }
   fillMaxWidth(value = true): this { return this.setProperty('fillMaxWidth', value); }
   fillMaxHeight(value = true): this { return this.setProperty('fillMaxHeight', value); }
   fillMaxSize(value = true): this { return this.setProperty('fillMaxSize', value); }
-
-  addElement(element: SduiElement): this { this.elements.push(element); return this; }
+  background(value: StackGroupProperties['background']): this { return this.setProperty('background', value); }
+  border(value: StackGroupProperties['border']): this { return this.setProperty('border', value); }
+  shape(value: StackGroupProperties['shape']): this { return this.setProperty('shape', value); }
 
   build(): SduiGroup {
     return GroupFactory.create('stack_group', {
       id: this.id,
       properties: this.propertiesSnapshot(),
-      elements: this.elements,
+      elements: this.buildElements(),
     });
   }
 }
