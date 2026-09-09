707a1c3a946ba2728c488e07738bb5f74a4df125
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-09 23:03:10 +0530

    feat(sdui): complete text semantic builder API

--- sdui/ui-sdk/src/definitions/elements/text/text.builder.ts
+++ sdui/ui-sdk/src/definitions/elements/text/text.builder.ts
@@ -1,4 +1,5 @@
 import type { SduiElement } from '../../../contract/element.schema.js';
+import type { Accessory } from '../../../properties/accessory/accessory.schema.js';
 import { TypedElementBuilder } from '../../../builder/TypedElementBuilder.js';
 import { ElementFactory } from '../../../factory/NodeFactories.js';
 import type { TextProperties } from './text.properties.js';
@@ -13,11 +14,19 @@ export class TextBuilder extends TypedElementBuilder<TextProperties> {
   fontSize(value: number): this { return this.setProperty('fontSize', value); }
   fontWeight(value: number): this { return this.setProperty('fontWeight', value); }
   bold(): this { return this.setProperty('fontWeight', 700); }
+  lineHeight(value: number): this { return this.setProperty('lineHeight', value); }
+  letterSpacing(value: number): this { return this.setProperty('letterSpacing', value); }
   color(value: TextProperties['color']): this { return this.setProperty('color', value); }
   textAlign(value: TextProperties['textAlign']): this { return this.setProperty('textAlign', value); }
+  width(value: TextProperties['width']): this { return this.setProperty('width', value); }
+  height(value: TextProperties['height']): this { return this.setProperty('height', value); }
+  maxWidth(value: TextProperties['maxWidth']): this { return this.setProperty('maxWidth', value); }
+  weight(value: TextProperties['weight']): this { return this.setProperty('weight', value); }
   fillMaxWidth(value = true): this { return this.setProperty('fillMaxWidth', value); }
   leading(value: TextProperties['leading']): this { return this.setProperty('leading', value); }
   trailing(value: TextProperties['trailing']): this { return this.setProperty('trailing', value); }
+  leadingDivider(properties: Accessory['properties']): this { return this.leading([{ type: 'divider', properties }]); }
+  trailingDivider(properties: Accessory['properties']): this { return this.trailing([{ type: 'divider', properties }]); }
 
   build(): SduiElement { return ElementFactory.create('text', this.elementInput()); }
 }
