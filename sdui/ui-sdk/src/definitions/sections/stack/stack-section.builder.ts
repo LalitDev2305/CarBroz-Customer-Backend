28373128c34a9bbe2370c816d47a3470e769ec40
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-09 23:01:27 +0530

    feat(sdui): make stack section own child builders

--- sdui/ui-sdk/src/definitions/sections/stack/stack-section.builder.ts
+++ sdui/ui-sdk/src/definitions/sections/stack/stack-section.builder.ts
@@ -1,13 +1,11 @@
-import type { SduiElement } from '../../../contract/element.schema.js';
-import type { SduiGroup } from '../../../contract/group.schema.js';
 import type { SduiSection } from '../../../contract/section.schema.js';
-import { TypedPropertyBuilder } from '../../../builder/TypedPropertyBuilder.js';
+import { ElementParentBuilder } from '../../../builder/ElementParentBuilder.js';
 import { SectionFactory } from '../../../factory/NodeFactories.js';
+import { StackGroupBuilder } from '../../groups/stack/stack-group.builder.js';
 import type { StackSectionProperties } from './stack-section.properties.js';
 
-export class StackSectionBuilder extends TypedPropertyBuilder<StackSectionProperties> {
-  private readonly elements: SduiElement[] = [];
-  private readonly groups: SduiGroup[] = [];
+export class StackSectionBuilder extends ElementParentBuilder<StackSectionProperties> {
+  private readonly groupBuilders: StackGroupBuilder[] = [];
   private orientation: 'vertical' | 'horizontal' = 'vertical';
 
   constructor(private readonly id: string) { super(); }
@@ -24,25 +22,38 @@ export class StackSectionBuilder extends TypedPropertyBuilder<StackSectionProper
       ? this.setProperty('horizontalAlignment', 'center')
       : this.setProperty('verticalAlignment', 'center');
   }
+  horizontalAlignment(value: StackSectionProperties['horizontalAlignment']): this { return this.setProperty('horizontalAlignment', value); }
+  verticalAlignment(value: StackSectionProperties['verticalAlignment']): this { return this.setProperty('verticalAlignment', value); }
   padding(value: StackSectionProperties['padding']): this { return this.setProperty('padding', value); }
+  width(value: StackSectionProperties['width']): this { return this.setProperty('width', value); }
+  height(value: StackSectionProperties['height']): this { return this.setProperty('height', value); }
+  maxWidth(value: StackSectionProperties['maxWidth']): this { return this.setProperty('maxWidth', value); }
+  weight(value: StackSectionProperties['weight']): this { return this.setProperty('weight', value); }
   fillMaxWidth(value = true): this { return this.setProperty('fillMaxWidth', value); }
   fillMaxHeight(value = true): this { return this.setProperty('fillMaxHeight', value); }
   fillMaxSize(value = true): this { return this.setProperty('fillMaxSize', value); }
+  background(value: StackSectionProperties['background']): this { return this.setProperty('background', value); }
+  border(value: StackSectionProperties['border']): this { return this.setProperty('border', value); }
+  shape(value: StackSectionProperties['shape']): this { return this.setProperty('shape', value); }
 
-  addElement(element: SduiElement): this {
-    if (this.groups.length) throw new Error('StackSectionBuilder cannot contain both elements and groups');
-    this.elements.push(element);
-    return this;
+  protected override beforeAddElement(): void {
+    if (this.groupBuilders.length > 0) {
+      throw new Error('StackSectionBuilder cannot contain both elements and groups');
+    }
   }
 
-  addGroup(group: SduiGroup): this {
-    if (this.elements.length) throw new Error('StackSectionBuilder cannot contain both elements and groups');
-    this.groups.push(group);
-    return this;
+  addStackGroup(id: string): StackGroupBuilder {
+    if (this.hasElements()) throw new Error('StackSectionBuilder cannot contain both elements and groups');
+    const group = new StackGroupBuilder(id);
+    this.groupBuilders.push(group);
+    return group;
   }
 
   build(): SduiSection {
-    const content = this.groups.length ? { groups: this.groups } : { elements: this.elements };
+    const content = this.groupBuilders.length > 0
+      ? { groups: this.groupBuilders.map((builder) => builder.build()) }
+      : { elements: this.buildElements() };
+
     return SectionFactory.create('stack_section', {
       id: this.id,
       properties: this.propertiesSnapshot(),
