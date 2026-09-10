f6d1e56f475c6ca5309c20de203ee9c86c5f48df
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-10 11:14:01 +0530

    feat(sdui): register canonical form template definition

--- sdui/engine/src/nodes/definitions.ts
+++ sdui/engine/src/nodes/definitions.ts
@@ -6,11 +6,13 @@ import { InputDefinition } from './element/Input.js';
 import { TextDefinition } from './element/Text.js';
 import { StackGroupDefinition } from './group/StackGroup.js';
 import { StackSectionDefinition } from './section/StackSection.js';
+import { FormTemplateDefinition } from './template/FormTemplate.js';
 import { StackTemplateDefinition } from './template/StackTemplate.js';
 
 /** Canonical reusable SDUI vocabulary currently required by the engine migration. */
 export const productionNodeDefinitions: readonly NodeDefinition[] = Object.freeze([
   StackTemplateDefinition,
+  FormTemplateDefinition,
   StackComponentDefinition,
   StackSectionDefinition,
   StackGroupDefinition,
